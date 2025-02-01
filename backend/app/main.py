accfd5e695a268909c2aae9627a0bb7d45467214
Reece Roskam <roskamre@msu.edu>
2025-02-01 19:34:47 +0000

    working api and binaries

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 from app.api.endpoints import router
+from dotenv import load_dotenv
+import os
+import openai
+
+# Initialize the OpenAI client
 
 app = FastAPI(title="Query Optimizer API")
-
+load_dotenv()
+api_key = os.getenv('OPENAI_API_KEY')
+openai.api_key = api_key
 # ✅ Ensure the API router is included
 app.include_router(router, prefix="/api")
 
